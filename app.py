0969b026337fa7883e538116b10dcbf4caa06ba0
Dan Staines <dstaines@ebi.ac.uk>
2017-08-01 14:46:13 +0100

    added async email alerting using celery

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from sqlalchemy import create_engine, text
 from ensembl_prodinf import HiveInstance
+from ensembl_prodinf.tasks import email_when_complete
 import json
 import logging
 import re
@@ -23,10 +24,16 @@ json_pattern = re.compile("application/json")
 @app.route('/submit', methods=['POST'])
 def submit():
 
-    print request.headers['Content-Type']
     if json_pattern.match(request.headers['Content-Type']):
+        logging.debug("Submitting HC "+str(request.json))
         job = hive.create_job(analysis, request.json)
-        return jsonify({"job_id":job.job_id});
+        results = {"job_id":job.job_id};
+        email =  request.json['email']
+        if email != None and email != '':
+            logging.debug("Submitting email request for  "+email)
+            email_results = email_when_complete.delay(request.url_root+"results_email/"+str(job.job_id),email)
+            results['email_task'] = email_results.id
+        return jsonify(results);
     else:
         return "Could not handle input of type "+request.headers['Content-Type'], 415
 
@@ -36,6 +43,28 @@ def results(job_id):
     try:
         logging.info("Retrieving job with ID "+str(job_id))
         return jsonify(hive.get_result_for_job_id(job_id))
+    except ValueError:
+        return "Job "+str(job_id)+" not found", 404
+
+@app.route('/results_email/<int:job_id>', methods=['GET'])
+def results_email(job_id):
+    try:
+        email = request.args.get('email')
+        logging.info("Retrieving job with ID "+str(job_id)+" for "+str(email))
+        results = hive.get_result_for_job_id(job_id)
+        if results['status'] == 'complete':
+            results['subject'] = 'Healthchecks for %s - %s' % (results['output']['db_name'], results['output']['status'])
+            results['body'] = "Results for %s:\n" % (results['output']['db_uri'])
+            for (test,result) in results['output']['results'].iteritems():
+                results['body'] += "* %s : %s\n" % (test, result['status'])
+                if result['messages'] != None:
+                    for msg in result['messages']:
+                        results['body'] += "** %s\n" % (msg)
+        elif results['status'] == 'failed':
+            results['subject'] = 'Healthcheck job failed'
+            results['body'] = 'HC job failed. Boo.'
+        results['output'] = None
+        return jsonify(results)
     except ValueError:
         return "Job "+str(job_id)+" not found", 404
 
