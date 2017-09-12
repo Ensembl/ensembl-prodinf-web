bafc2e6395f1e1ed7233f26bb985b9344c9cc800
Dan Staines <dstaines@ebi.ac.uk>
2017-09-12 12:13:06 +0100

    added placeholder for db copying

---
+++ app/scripts/controllers/copy.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ensemblProdinfHcserviceApp.controller:MainCtrl
+ * @description # MainCtrl Controller of the ensemblProdinfHcserviceApp
+ */
+
+
+angular.module('hcSrvApp')
+    .controller('CopyCtrl', function ($scope, $http, $routeParams, CONFIG) {
+		
+    }
+    	);
