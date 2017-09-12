6f875a4e708f2457395da15d72339d8becdc6580
Dan Staines <dstaines@ebi.ac.uk>
2017-09-12 12:11:23 +0100

    make function for loading callable

--- app/scripts/controllers/list.js
+++ app/scripts/controllers/list.js
@@ -9,13 +9,18 @@
 
 angular.module('hcSrvApp')
     .controller('ListCtrl', function ($scope, $http, $routeParams, CONFIG) {
-		var url = CONFIG.HC_SRV_URL+'jobs';
+		
+		$scope.loadJobs = function() {
+			var url = CONFIG.HC_SRV_URL+'jobs';
 		$http.get(url)
 		    .then(function(response) {
 			$scope.jobs = response.data;
 		    }).catch(function (data) {	
 			console.log(data);
-			window.alert('Could not get result for job');
+			window.alert('Could not get jobs');
 		    });
+		};
+		$scope.loadJobs();
+		
     }
     	);
